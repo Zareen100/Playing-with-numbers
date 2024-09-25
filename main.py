46ad25f7e9996ad7fb48d1ded35d1dee13a1167e
Zareen100 <monzu001976@gmail.com>
2024-09-25 21:50:23 +0600

    Playing with numbers

---
+++ main.py
@@ -0,0 +1,32 @@
+# #activity 1
+# num=int(input("enter no"))
+# originalnum=num
+# reversednum=0
+# while num>0:
+#     digit=num%10
+#     reversednum=reversednum*10+digit
+#     num//=10
+# if originalnum==reversednum:
+#     print(originalnum,"It is a pallindrome")
+# else:
+#     print(originalnum,"It is not a palindrome")
+# num=int(input("enter a number"))
+# if num>0:
+#     print("it is a positive number",num)
+# else:
+#     print("It is not a positive number",num)
+#activity 2
+# numlargest=int(input("enter a number"))
+# numsmallest=int(input("enter a number"))
+# while numsmallest:
+#     numberstore=numsmallest
+#     numsmallest=numlargest%numsmallest
+#     numlargest=numberstore
+# print("HCF is",numlargest)
+num=10
+if num%2==0:
+    print("it is an even number")
+else:
+    print("it is not an even number")
+
+    
